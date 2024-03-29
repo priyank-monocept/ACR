bfe4078599d26f991298ce107f91e282fd5b0da3
Priyank Jain <priyank@monocept.com>
2024-03-29 10:52:12 +0530

    handled code

--- frontend/src/components/ACRFormFields.js
+++ frontend/src/components/ACRFormFields.js
@@ -3,7 +3,7 @@ import { useForm, Controller } from "react-hook-form";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { DevTool } from "@hookform/devtools";
-
+import axios from "axios";
 const ACRFormFields = () => {
   const {
     register,
@@ -13,7 +13,31 @@ const ACRFormFields = () => {
   } = useForm();
 
   const formSubmit = (data) => {
-    console.log(data);
+
+    axios({
+      method: 'post',
+      url: '/generate-pdf', // Route on your Node.js server
+      responseType: 'blob', //Important
+      data, 
+    }).then(response => {
+      // Create a blob from the response data
+     const blob = new Blob([response.data], { type: 'application/pdf' });
+      // Create a link element to download the PDF
+      const link = document.createElement('a');
+      link.href = window.URL.createObjectURL(blob);
+      link.download = 'example.pdf'; // File name
+      // Append the link to the body
+      document.body.appendChild(link);
+      // Trigger the download
+      link.click();
+      // Clean up
+      document.body.removeChild(link);
+      
+     console.log(response);
+    }).catch(error => {
+      console.error('Error generating PDF:', error);
+    });
+
   };
 
   const getValidationRules = (options) => ({
